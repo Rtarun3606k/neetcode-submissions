51981514a895ef8b9292ac54e7487afe02c600fd
Tarun Nayaka R <97576326+Rtarun3606k@users.noreply.github.com>
2026-06-12 14:02:53 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.go
@@ -0,0 +1,25 @@
+func isPalindrome(s string) bool {
+    l, r := 0, len(s)-1
+
+    for l < r {
+        for l < r && !isAlphaNum(rune(s[l])) {
+            l++
+        }
+        for l < r && !isAlphaNum(rune(s[r])) {
+            r--
+        }
+
+        if unicode.ToLower(rune(s[l])) != unicode.ToLower(rune(s[r])) {
+            return false
+        }
+
+        l++
+        r--
+    }
+
+    return true
+}
+
+func isAlphaNum(ch rune) bool {
+    return unicode.IsLetter(ch) || unicode.IsDigit(ch)
+}
