d199e8c7b67d826c803d45f83fd010fd4966c5b5
Tarun Nayaka R <97576326+Rtarun3606k@users.noreply.github.com>
2026-06-16 19:56:58 +0530

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.go
@@ -0,0 +1,40 @@
+type Solution struct{}
+
+func (s *Solution) Encode(strs []string) string {
+var result strings.Builder
+
+	for _,str := range strs{
+result.WriteString(strconv.Itoa(len(str)))
+	result.WriteString("#")
+	result.WriteString(str)
+	}
+
+	return result.String()
+}
+
+func (s *Solution) Decode(encoded string) []string {
+
+
+    result := []string{}
+
+    i := 0
+
+    for i < len(encoded) {
+        j := i
+
+        for encoded[j] != '#' {
+            j++
+        }
+
+        length, _ := strconv.Atoi(encoded[i:j])
+
+        word := encoded[j+1 : j+1+length]
+        result = append(result, word)
+
+        i = j + 1 + length
+    }
+
+    return result
+
+
+}
